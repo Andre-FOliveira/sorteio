2aca0add4b807727d2bf26755951941637021c7a
Andre-FOliveira <andre.faustino@hotmail.com>
2023-06-20 18:16:50 -0300

    Subindo Projeto

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+function generateNumber(){
+
+    const min = Math.ceil(document.querySelector('.input-min').value)
+
+    const max = Math.floor(document.querySelector('.input-max').value)
+
+
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    document.getElementById('res').innerHTML = result;
+
+    // alert('O número Sorteado é  ' + result);
+}
+
+
+
+
+
+
+
+
+
+
