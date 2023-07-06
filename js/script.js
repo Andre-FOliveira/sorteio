a58bfdb1271a09f0388575fde8e32f50139dd1b7
Andre-FOliveira <andre.faustino@hotmail.com>
2023-07-06 15:21:50 -0300

    Melhoria dos valores
    Ajsute na comparação dos valores, não deixando o usuario colocar o primeiro numero menor que o segundo

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ function generateNumber(){
 
     const max = Math.floor(document.querySelector('.input-max').value)
 
-
+    if(min >= max){
+        alert("O valor minimo dever ser MENOR que o valor maximo")
+    }else{
     let result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     document.getElementById('res').innerHTML = result;
-
+    }    
     // alert('O número Sorteado é  ' + result);
 }
 
